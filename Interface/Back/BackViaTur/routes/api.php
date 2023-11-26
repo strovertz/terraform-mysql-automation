c4517a5dcf7865eb3e941dae9ebcb867ec4cbd3c
postrenan <renan.bick@acad.ufsm.br>
2023-11-26 14:59:53 -0300

    pt

---
+++ Interface/Back/BackViaTur/routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\SupplierManagerController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::prefix('supplier')->group(function (){
+   Route::get('', [SupplierManagerController::class, 'index']);
+   Route::post('',[SupplierManagerController::class, 'store']);
+   Route::get('/{id}', [SupplierManagerController::class, 'update']);
+   Route::delete('/{id}',[SupplierManagerController::class, 'delete']);
+});
