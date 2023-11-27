ec756754fff38c8628346471106b2509412eb228
postrenan <renan.bick@acad.ufsm.br>
2023-11-27 01:22:01 -0300

    pt

--- Interface/Front/ViaTur/src/views/LoginView.vue
+++ Interface/Front/ViaTur/src/views/LoginView.vue
@@ -1,7 +1,14 @@
 <template>
   <main>
-    <div class="section columns is-fullheight  is-centered">
-      <div v-if="!nextStep" id="main" class="column is-half  box has-text-centered">
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <a class="navbar-item">
+          <h1 class="title"><strong>ViaTur</strong><br>Sua melhor escolha para turismo</h1>
+        </a>
+      </div>
+    </nav>
+    <div v-if="creatPanel===0" class="section columns is-fullheight  is-centered">
+      <div id="main" class="column is-half  box has-text-centered">
         <h2>Login</h2>
         <br>
         <div>
@@ -20,71 +27,77 @@
         </div>
         <div>
           <br>
-          <button class="button is-rounded is-light" @click="loginValidation">Logar</button>
+          <button class="button is-rounded is-light" @click="">Logar</button>
         </div>
         <div>
           <br>
-          <button @click="createAccountStep" class="button is-rounded is-light " >criar uma conta </button>
+          <button @click="createAccountStep" class="button is-rounded is-light ">criar uma conta</button>
 
         </div>
       </div>
-      <div v-else-if="nextStep">
 
-      </div>
     </div>
-    <nav class="navbar" role="navigation" aria-label="main navigation">
-      <div class="navbar-brand">
-        <a class="navbar-item">
-          <h1 class="title"><strong>ViaTur</strong><br>Sua melhor escolha para turismo</h1>
-        </a>
-      </div>
-    </nav>
 
-    <div class="box">
+
+    <div class="box" v-if="creatPanel !== 0">
       <div class="box">
-        <h3>Preencha seus dados para a compra</h3>
-        <form>
-          <div class="field">
-            <label class="label">Nome completo</label>
-            <div class="control">
-              <input class="input" type="text" v-model="nameUser" placeholder="Diga o nome da sua empresa">
+        <h3 class="title">Preencha seus dados para logar</h3>
+        <div class="field">
+          <label class="label">Nome completo</label>
+          <div class="control">
+            <input class="input" type="text" v-model="nameUser" placeholder="Diga o nome da sua empresa">
+          </div>
+        </div>
+
+        <div class="field">
+          <label class="label">Genero</label>
+          <div class="control">
+            <div class="select">
+              <select v-model="GenderSelectedOption">
+                <option>Feminino</option>
+                <option>Masculino</option>
+              </select>
             </div>
           </div>
+        </div>
 
-          <div class="field">
-            <label class="label">Genero</label>
-            <div class="control">
-              <div class="select">
-                <select v-model="GenderSelectedOption">
-                  <option>Feminino</option>
-                  <option>Masculino</option>
-                </select>
-              </div>
-            </div>
+        <div class="field">
+          <label class="label">CPF</label>
+          <div class="control">
+            <input class="input" v-model="cpf" type="text" placeholder="Digite o valor">
           </div>
+        </div>
 
-          <div class="field">
-            <label class="label">CPF</label>
-            <div class="control">
-              <input class="input" v-model="cpf" type="text" placeholder="Digite o valor">
-            </div>
+        <div class="field">
+          <label class="label">Endereço</label>
+          <div class="control">
+            <input class="input" type="text" v-model="addressUser" placeholder="Digite o endereço">
           </div>
+        </div>
 
-          <div class="field">
-            <label class="label">Endereço</label>
-            <div class="control">
-              <input class="input" type="text" v-model="addressUser" placeholder="Digite o endereço">
-            </div>
+        <div class="field">
+          <label class="label">Data de Nascimento</label>
+          <div class="control">
+            <input class="input" type="date" v-model="birthday" placeholder="Digite a data">
           </div>
+        </div>
 
-          <div class="field">
-            <label class="label">Data de Nascimento</label>
-            <div class="control">
-              <input class="input" type="date" v-model="birthday" placeholder="Digite a data">
-            </div>
+        <div class="field">
+          <label class="label">Email</label>
+          <div class="control">
+            <input class="input" v-model="email" type="text" placeholder="Digite o valor">
           </div>
+        </div>
 
-        </form>
+        <div class="field">
+          <label class="label">Senha</label>
+          <div class="control">
+            <input class="input" v-model="password" type="text" placeholder="Digite o valor">
+          </div>
+        </div>
+
+
+        <button class="button">Criar Conta</button>
       </div>
     </div>
   </main>
@@ -92,6 +105,7 @@
 
 <script>
 import {instance} from '@/main';
+
 export default {
   name: 'home',
 
@@ -102,8 +116,14 @@ export default {
       cpf: '',
       addressUser: '',
       nameUser: '',
+      creatPanel: 0,
     }
   },
+  methods: {
+    createAccountStep: function () {
+      this.creatPanel = 1;
+    }
+  }
 }
 </script>
 
