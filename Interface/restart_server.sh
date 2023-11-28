62c3066dc63ca3112da7b030f39251cfb863072c
Gleison Pires <74078237+strovertz@users.noreply.github.com>
2023-11-28 20:23:13 -0300

    Update restart_server.sh

--- Interface/restart_server.sh
+++ Interface/restart_server.sh
@@ -1,1 +1,11 @@
+#!bin/bash
 
+echo cd /var/www/html/Interface/Front/ViaTur
+
+echo nohup npm run dev -- --host &
+
+echo cd /var/www/html/Interface/Back/BackViaTur
+
+echo nohup php artisan serve &
+
+echo "Servidores Iniciados"
