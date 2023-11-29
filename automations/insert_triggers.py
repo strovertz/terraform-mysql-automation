78ec3022dedc80d92b97ec518c115f2fe2232af8
Gleison Pires <74078237+strovertz@users.noreply.github.com>
2023-11-28 22:06:19 -0300

    Update insert_triggers.py

--- automations/insert_triggers.py
+++ automations/insert_triggers.py
@@ -6,59 +6,51 @@ db_config = {
     'password': 'root',
     'database': 'agencia_turismo'
 }
+
 def create_triggers():
     conn = mysql.connector.connect(**db_config)
     cursor = conn.cursor()
 
     # Trigger para a tabela clientes
     cursor.execute("""
-        DELIMITER //
         CREATE TRIGGER clientes_before_update
         BEFORE UPDATE ON clientes
         FOR EACH ROW
         BEGIN
-            -- da block naa atualização se o email for alterado
+            -- Impede a atualização se o email for alterado
             IF NEW.email <> OLD.email THEN
                 SIGNAL SQLSTATE '45000'
                 SET MESSAGE_TEXT = 'Não é permitido alterar o email do cliente.';
             END IF;
         END;
-        //
-        DELIMITER ;
     """)
 
     # Trigger para a tabela servicos
     cursor.execute("""
-        DELIMITER //
         CREATE TRIGGER servicos_before_insert
         BEFORE INSERT ON servicos
         FOR EACH ROW
         BEGIN
-            -- n deixa que ocorra a inserção de serviços com valor igual a zero
+            -- Impede a inserção de serviços com valor igual a zero
             IF NEW.valor = 0 THEN
                 SIGNAL SQLSTATE '45000'
                 SET MESSAGE_TEXT = 'Não é permitido inserir serviços com valor igual a zero.';
             END IF;
         END;
-        //
-        DELIMITER ;
     """)
 
     # Trigger para a tabela itens
     cursor.execute("""
-        DELIMITER //
         CREATE TRIGGER itens_before_delete
         BEFORE DELETE ON itens
         FOR EACH ROW
         BEGIN
-            -- nao deixa acontecer a exclusão se o valor do produto for maior que 100
+            -- Cancela a exclusão se o valor do produto for maior que 100
             IF OLD.valor_produto > 100 THEN
                 SIGNAL SQLSTATE '45000'
                 SET MESSAGE_TEXT = 'Não é permitido excluir itens com valor maior que 100.';
             END IF;
         END;
-        //
-        DELIMITER ;
     """)
 
     conn.commit()
@@ -67,6 +59,6 @@ def create_triggers():
 def main():
     create_triggers()
     
-    print('triggers criados com sucesso!')
+    print('Triggers criados com sucesso!')
 
 main()
